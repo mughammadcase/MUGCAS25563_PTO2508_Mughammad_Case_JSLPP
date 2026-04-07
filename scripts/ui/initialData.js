54ddce7a8c928508a5e8f7c27a6ad41b17596e19
Mughammad <caseameen44@gmail.com>
2026-04-08 01:02:21 +0200

    Add initial task data
    - create static dataset for initial board state

--- scripts/ui/initialData.js
+++ scripts/ui/initialData.js
@@ -1,1 +1,50 @@
 // Fallback data for the UI, in case the API is not available
+
+export const initialTasks = [
+  {
+    id: 1,
+    title: "Launch Epic Career 🚀",
+    description: "Create a killer Resume",
+    status: "todo",
+    board: "Launch Career",
+  },
+  {
+    id: 2,
+    title: "Master JavaScript 💛",
+    description: "Get comfortable with the fundamentals",
+    status: "doing",
+    board: "Launch Career",
+  },
+  {
+    id: 3,
+    title: "Keep on Going 🏆",
+    description: "You're almost there",
+    status: "doing",
+    board: "Launch Career",
+  },
+
+  {
+    id: 11,
+    title: "Learn Data Structures and Algorithms 📚",
+    description:
+      "Study fundamental data structures and algorithms to solve coding problems efficiently",
+    status: "todo",
+    board: "Launch Career",
+  },
+  {
+    id: 12,
+    title: "Contribute to Open Source Projects 🌐",
+    description:
+      "Gain practical experience and collaborate with others in the software development community",
+    status: "done",
+    board: "Launch Career",
+  },
+  {
+    id: 13,
+    title: "Build Portfolio Projects 🛠️",
+    description:
+      "Create a portfolio showcasing your skills and projects to potential employers",
+    status: "done",
+    board: "Launch Career",
+  },
+];
